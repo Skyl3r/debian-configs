5c821f4d842ed56cffb961e6731d370061ba7dc7
Skyl3r <kpereksta@gmail.com>
2021-02-15 10:46:07 -0500

    Modified vim setup script to work in new repo structure

--- scripts/vim-setup.sh
+++ scripts/vim-setup.sh
@@ -6,8 +6,8 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Set configuration
-cp .vimrc ~/.vimrc
+cp ../.vimrc ~/.vimrc
 
 # Set theme
 mkdir ~/.vim/colors
-cp apprentice.vim ~/.vim/colors
+cp ../apprentice.vim ~/.vim/colors
