918342a45cad1b7e026d37588695f67631ce952b
Skyl3r <kpereksta@gmail.com>
2021-02-17 13:12:04 -0500

    Fixed key install

--- scripts/install-skyl3r-repo.sh
+++ scripts/install-skyl3r-repo.sh
@@ -4,7 +4,7 @@
 
 
 echo "Adding gpg key."
-wget -O - http://deb.skyl3r.com/debian/conf/deb.skyl3r.gpg.key | sudo apt-key add -
+wget -O - http://deb.skyl3r.com/deb.skyl3r.com.gpg.key | sudo apt-key add -
 echo "Adding repository"
 sudo touch /etc/apt/sources.list.d/deb.skyl3r.com.list
 sudo sh -c 'echo "deb http://deb.skyl3r.com/debian unstable main" > /etc/apt/sources.list.d/deb.skyl3r.com.list'
